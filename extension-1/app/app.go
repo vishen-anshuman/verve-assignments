282251e6a6c06a923d1b2a5ddb9457bea58d1231
Vanshuman <anshuman.vishen@repute.net>
2024-12-07 22:47:35 +0530

    Modifying Extension-1 Codes

--- extension-1/app/app.go
+++ extension-1/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"extension-1/logger"
 	"log"
 	"sync"
 )
@@ -10,4 +11,19 @@ type App struct {
 	UniqueIDCache  map[string]struct{}
 	MinuteLogger   *log.Logger
 	ShutdownSignal chan struct{}
+}
+
+var appConst *App
+
+func InitApp() {
+	appConst = &App{
+		Mu:             sync.Mutex{},
+		UniqueIDCache:  make(map[string]struct{}),
+		MinuteLogger:   logger.InitLogger(),
+		ShutdownSignal: make(chan struct{}),
+	}
+}
+
+func GetAppConst() *App {
+	return appConst
 }
